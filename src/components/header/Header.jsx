28fc991c8936acec71f1ce0e32440c80695ea393
VladAir <vladair@MacBook-Air-VladAir.local>
2022-11-28 19:08:04 +0300

    header 2/3 drop-down menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { HeaderContainer, BlockAddTasks, BlockUserProfil } from './Style';
+import React, { useState } from 'react';
+import {
+  HeaderContainer,
+  BlockAddTasks,
+  BlockUserProfil,
+  ContainerButtonAdd,
+  BlockButtonAdd,
+  ByttonAdd,
+  ButtonIcon,
+  ButtonSpan
+
+} from './Style';
 
 export default function Header() {
+  const [addTask, setAddtask] = useState(false);
+  const submitHandler = () => setAddtask(!addTask);
+  console.log(addTask);
   return (
     <HeaderContainer>
-      <BlockAddTasks />
-      <BlockUserProfil />
+      <BlockAddTasks>
+        <ContainerButtonAdd>
+          <BlockButtonAdd>
+            <ByttonAdd onClick={submitHandler}>
+              <ButtonIcon />
+              <ButtonSpan>Add new</ButtonSpan>
+            </ByttonAdd>
+          </BlockButtonAdd>
+        </ContainerButtonAdd>
+      </BlockAddTasks>
+      <BlockUserProfil>
+        BlockUserProfil
+      </BlockUserProfil>
     </HeaderContainer>
   );
 }
--- src/components/header/Style.js
+++ src/components/header/Style.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
+import iconBTB from './img/Plus.svg';
 
 export const HeaderContainer = styled.header`
     width: 100%;
@@ -23,6 +24,8 @@ export const BlockAddTasks = styled.div`
     background-color: tomato;
 `;
 
+//  <<------------ BlockUserProfil ------------>>
+
 export const BlockUserProfil = styled.div`
     background-color: yellow;
     flex: 1 1 auto;
@@ -34,3 +37,110 @@ export const BlockUserProfil = styled.div`
     align-items: center;
     padding: 0px 15px;
 `;
+
+export const ContainerButtonAdd = styled.div`
+   margin-right: 15px;
+`;
+
+export const BlockButtonAdd = styled.div`
+    pointer-events: all;
+`;
+
+export const ByttonAdd = styled.button`
+    margin: 0px;
+    border: 0px;
+    border-radius: 5px;
+    outline: none;
+    font-size: 14px;
+    transition: background 0.3s ease-out 0s;
+    padding: 0px 15px;
+    height: 38px;
+    min-width: 76px;
+    cursor: pointer;
+    color: rgb(255, 255, 255);
+    background: rgb(0, 148, 255);
+    font-family: Roboto, sans-serif !important;
+    line-height: 42px;
+    border-radius: 21px;
+    padding: 0px 0px 0px 16px;
+    display: flex;
+    flex-direction: row;
+    -webkit-box-align: center;
+    align-items: center;
+    text-align: left;
+    letter-spacing: 0px;
+    width: 107px;
+`;
+
+export const ButtonIcon = styled.svg`
+   &{
+     background: url(${iconBTB}) no-repeat;
+     width: 1em;
+     height: 1em;
+     color: rgb(255, 255, 255);
+     background-position: center;
+   }
+   &svg:not(:root) {
+    overflow: hidden;
+}
+`;
+
+export const ButtonSpan = styled.span`
+    margin-left: 7px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 23px;
+    color: rgb(255, 255, 255);
+`;
+
+// << ------------- drop-down menu ------------- >>
+export const ContainerAddTask = styled.div`
+    z-index: 10;
+    transform-origin: 54px 0px;
+    left: 389px;
+    top: 65px;
+`;
+export const BlockAddTask = styled.div`
+    display: block;
+`;
+export const FlexAddTask = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-width: 157px;
+    min-width: 157px;
+    overflow: auto;
+    z-index: 11;
+    margin-top: 5px;
+    margin-bottom: 5px;
+`;
+export const AddTask = styled.div`
+    color: rgb(34, 34, 34);
+    cursor: pointer;
+    line-height: 16px;
+    padding: 10px 15px 10px 12px;
+    font-size: 14px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    margin: 0px 8px;
+    border-radius: 8px;
+    background: transparent;
+`;
+
+export const AddEvent = styled.div`
+    color: rgb(34, 34, 34);
+    cursor: pointer;
+    line-height: 16px;
+    padding: 10px 15px 10px 12px;
+    font-size: 14px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    margin: 0px 8px;
+    border-radius: 8px;
+    background: transparent;
+`;
+
+// << ------------- drop-down menu ------------- >>
+
+//  <<------------ BlockUserProfil ------------>>
